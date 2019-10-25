54c82fb80055917c2faa8564057d04ca4430b949
Nelson Huang <xinchin@gmail.com>
2019-10-25 15:57:35 +0800

    - write a demo for test element-ui

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import DemoElementUI from '@/Demo/DemoElementUI'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes: [{
+    path: '/',
+    name: 'HelloWorld',
+    component: HelloWorld
+  }, {
+    path: '/demoElementUI',
+    name: 'demoElementUI',
+    component: DemoElementUI
+  }]
 })
